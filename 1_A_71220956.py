98fcc578b1a325c64e91359c56878a96f4b84e63
Vicky Yohanes <59169204+vickyyohanes@users.noreply.github.com>
2022-11-25 12:50:33 +0700

    Update 1_A_71220956.py

--- 1_A_71220956.py
+++ 1_A_71220956.py
@@ -2,7 +2,7 @@ nama = input("Masukkan nama mahasiswa : ")
 nim = input("Masukkan NIM-nya : ")
 a = int(nim[2:4])
 b = int(nim[0:2])
-if  a == 20 or a == 22 or a ==21 and d ==71 :
+if  a == 20 or a == 22 or a ==21 and b ==71 :
     print(nama," merupakan mahasiwa informatika angkatan 20 hingga 21")
 else    :
     print(nama," bukan merupakan mahasiswa informatika angkatan 20 hingga 21")
